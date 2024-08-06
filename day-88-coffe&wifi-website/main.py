69cea060fdcfd9576a6ad8d3e039fde75baade0b
sumeyyesever <sumeysever@gmail.com>
2024-08-06 23:19:19 +0300

    final commit

--- day-88-coffe&wifi-website/main.py
+++ day-88-coffe&wifi-website/main.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap5
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Float, Boolean
+from sqlalchemy import Integer, String, Boolean
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
 
 
 class Base(DeclarativeBase):
@@ -39,6 +45,20 @@ with app.app_context():
     db.create_all()
 
 
+class MyForm(FlaskForm):
+    name = StringField(label="Cafe Name", validators=[DataRequired()])
+    location = StringField(label="Cafe Location", validators=[DataRequired()])
+    map_url = StringField(label="Cafe Map URL", validators=[DataRequired()])
+    img_url = StringField(label="Cafe Image URL", validators=[DataRequired()])
+    coffee_price = StringField(label="Coffee Price", validators=[DataRequired()])
+    seats = StringField(label="Cafe Seats Number", validators=[DataRequired()])
+    has_toilet = BooleanField('Restrooms')
+    has_wifi = BooleanField('Wifi')
+    has_sockets = BooleanField('Sockets')
+    can_take_calls = BooleanField('Take Calls')
+    submit = SubmitField("Submit Cafe")
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -55,6 +75,29 @@ def show_cafes():
 def show_single_cafe(cafe_id):
     requested_cafe = db.get_or_404(Cafe, cafe_id)
     return render_template("cafe.html", cafe=requested_cafe)
+
+
+@app.route("/new-cafe", methods=["GET", "POST"])
+def add_new_cafe():
+    form = MyForm()
+    if request.method == "POST":
+
+        new_cafe = Cafe(name=form.name.data, location=form.location.data, img_url=form.img_url.data,
+                        map_url=form.map_url.data, coffee_price=form.coffee_price.data, seats=form.seats.data,
+                        has_toilet=form.has_toilet.data, has_sockets=form.has_sockets.data, has_wifi=form.has_wifi.data,
+                        can_take_calls=form.can_take_calls.data)
+        db.session.add(new_cafe)
+        db.session.commit()
+        return redirect(url_for("show_cafes"))
+    return render_template("add-cafe.html", form=form)
+
+
+@app.route("/delete-cafe/<int:delete_cafe_id>")
+def delete_cafe(delete_cafe_id):
+    the_cafe = db.get_or_404(Cafe, delete_cafe_id)
+    db.session.delete(the_cafe)
+    db.session.commit()
+    return redirect(url_for("show_cafes"))
 
 
 if __name__ == "__main__":
